ecd68928afbd3833ef06b449d12d5fb1262e66a7
0xrxL <127248639+0xrxL@users.noreply.github.com>
2026-06-23 07:42:53 +0200

    fix(YouTube - Disable player popup panels): Deprecated the old way to disable popup panels

--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/hide/player/popup/DisablePlayerPopupPanelsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/hide/player/popup/DisablePlayerPopupPanelsPatch.kt
@@ -1,8 +1,10 @@
 package app.morphe.patches.youtube.layout.hide.player.popup
 
+import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patches.shared.misc.settings.preference.SwitchPreference
+import app.morphe.patches.youtube.layout.captions.StartVideoInformerFingerprint
 import app.morphe.patches.youtube.misc.extension.sharedExtensionPatch
 import app.morphe.patches.youtube.misc.settings.PreferenceScreen
 import app.morphe.patches.youtube.misc.settings.settingsPatch
@@ -29,10 +31,15 @@ val disablePlayerPopupPanelsPatch = bytecodePatch(
             SwitchPreference("morphe_disable_player_popup_panels", summary = true),
         )
 
+        StartVideoInformerFingerprint.method.addInstruction(
+            0,
+            "invoke-static { }, $EXTENSION_CLASS->enablePlayerPopupPanelsBypass()V"
+        )
+
         EngagementPanelControllerFingerprint.method.addInstructionsWithLabels(
             0,
             """
-                invoke-static/range { p3 .. p4 }, $EXTENSION_CLASS->disablePlayerPopupPanels(ZZ)Z
+                invoke-static/range { }, $EXTENSION_CLASS->disablePlayerPopupPanels()Z
                 move-result v0
                 if-eqz v0, :player_popup_panels
                 const/4 v0, 0x0
